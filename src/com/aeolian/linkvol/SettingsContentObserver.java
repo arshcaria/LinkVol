76cb7bba2ba2e5744284fa0c1f804e9447773463
arshcaria <aeolianliu@gmail.com>
2013-11-12 00:59:43 +0800

    code clean in SettingsContentObserver.java

--- src/com/aeolian/linkvol/SettingsContentObserver.java
+++ src/com/aeolian/linkvol/SettingsContentObserver.java
@@ -8,37 +8,36 @@ import android.util.Log;
 
 public class SettingsContentObserver extends ContentObserver {
 	private static final String TAG = "SettingsContentObserver";
-    int previousVolume;
-    Context context;
+	private int previousVolume;
+	private int currentVolume;
+	private Context mContext;
 
-    public SettingsContentObserver(Context c, Handler handler) {
-        super(handler);
-        context=c;
+	public SettingsContentObserver(Context c, Handler handler) {
+		super(handler);
+		mContext = c;
+		AudioManager audio = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+		previousVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+	}
 
-        AudioManager audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        previousVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
-    }
+	@Override
+	public boolean deliverSelfNotifications() {
+		return super.deliverSelfNotifications();
+	}
 
-    @Override
-    public boolean deliverSelfNotifications() {
-        return super.deliverSelfNotifications();
-    }
+	@Override
+	public void onChange(boolean selfChange) {
+		super.onChange(selfChange);
 
-    @Override
-    public void onChange(boolean selfChange) {
-        super.onChange(selfChange);
+		AudioManager audio = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+		currentVolume = audio.getStreamVolume(AudioManager.STREAM_RING);
 
-        AudioManager audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        int currentVolume = audio.getStreamVolume(AudioManager.STREAM_RING);
+		int delta = previousVolume - currentVolume;
 
-        int delta=previousVolume-currentVolume;
+		if (delta != 0) {
+			Log.d(TAG, "Ringtone volume changed to " + currentVolume);
+			previousVolume = currentVolume;
+			audio.setStreamVolume(AudioManager.STREAM_NOTIFICATION, currentVolume, 0);
+		}
 
-        if(delta != 0)
-        {
-            Log.d(TAG, "Ringtone volume changed to " + currentVolume);
-            previousVolume=currentVolume;
-            audio.setStreamVolume(AudioManager.STREAM_NOTIFICATION, currentVolume, 0);
-        }
-      
-    }
+	}
 }
